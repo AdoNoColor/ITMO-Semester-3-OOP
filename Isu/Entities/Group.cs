84dc9a71c8664b4d6c55959c1bbb9209874f27b4
AdoNoColor <90046840+AdoNoColor@users.noreply.github.com>
2021-10-15 18:11:13 +0300

    fix: validator

--- Isu/Entities/Group.cs
+++ Isu/Entities/Group.cs
@@ -26,8 +26,10 @@ namespace Isu.Entities
         private bool IsGroupNameCorrect(string name)
         {
             if (name.Length != 5) throw new IsuException("Name of the group does not follow certain rules. TIP: M3XYY");
-            return name[0] == 'M' && name[1] == '3' && (int)name[2] > 0 && (int)name[2] < 5 && (int)name[3] >= 0 && (int)name[3] <= 9 &&
-                   (int)name[4] >= 0 && (int)name[4] <= 9;
+            int groupNumber = (int.Parse(GroupName[3].ToString()) * 10) + int.Parse(GroupName[4].ToString());
+            return name[0] == 'M' && name[1] == '3' &&
+                   Enumerable.Range(1, 4).Contains(int.Parse(GroupName[2].ToString())) &&
+                   Enumerable.Range(0, 99).Contains(groupNumber);
         }
     }
 }
